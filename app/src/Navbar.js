1eed3e2680568ea86b03e79e8da052553bbb8cf3
RDG97 <ryangann1997@gmail.com>
2023-01-11 22:09:52 +0000

    links page fleshed out

--- app/src/Navbar.js
+++ app/src/Navbar.js
@@ -1,24 +1,42 @@
 import { Slide,Fade } from "react-awesome-reveal";
-export default function Navbar() {
+export default function Navbar(props) {
+
+
+
+function test() {}
+
+  
+  function pageAbout(){
+    props.setPage('aboutme')
+  }
+  function pageShowcase(){
+    props.setPage('showcase')
+  }
+  function pageContact(){
+    props.setPage('contactme')
+  }
+  function pageLinks(){
+    props.setPage('links')
+  }
 
     return (
         <>
         <Slide cascade>      
-            <ul className="nav flex-column bg-dark navver">
-                <h2 className="text-light">Ryan Gann</h2>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li className="nav-item">
-                    <p className="text-light" >About me</p>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
+        <ul className="nav flex-column bg-dark navver">
+              <h2 className="text-light">Ryan Gann</h2>
+              <li className="nav-item">
+              <p className="text-light" onClick={pageShowcase}>Showcase</p>
+              </li>
+              <li className="nav-item">
+                  <p className="text-light" onClick={pageAbout}>About me</p>
+              </li>
+              <li className="nav-item">
+              <p className="text-light" onClick={pageLinks}>Links</p>
+              </li>
+              <li className="nav-item">
+              <p className="text-light" onClick={test}>Contact me</p>
+              </li>
+          </ul>
             </Slide>
             </>
     );
